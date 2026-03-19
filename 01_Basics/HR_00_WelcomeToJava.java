5b344ec89533ce1ae4093ed49c7ee9ecac9ae154
Muhammad Abdullah <mabdullah20089@gmail.com>
2026-03-19 14:08:51 +0500

    Solved HR_00: Welcome To Java (basic output)

---
+++ 01_Basics/HR_00_WelcomeToJava.java
@@ -0,0 +1,15 @@
+/*Platform: Hackerrank
+ *Problem: Welcome To Java
+ *Day: 1
+ *Date: 19 / 03 / 2026
+ *Author: Muhammad Abdullah
+ */
+
+public class HR_00_WelcomeToJava {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
