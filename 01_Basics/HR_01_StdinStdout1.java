cadbb6822531c3d05b45188ad838d3f99214e81a
Muhammad Abdullah <mabdullah20089@gmail.com>
2026-03-20 14:55:19 +0500

    Solved HR_01: Stdin and Stdout (input/output basics)

---
+++ 01_Basics/HR_01_StdinStdout1.java
@@ -0,0 +1,24 @@
+/*
+Platform: HackerRank
+Track: Java - Introduction
+Problem: Java Stdin and Stdout I
+Difficulty: Easy
+Concepts: Input, Output, Scanner
+Date: 27-02-2026
+*/
+
+import java.util.Scanner;
+
+public class HR_01_StdinStdout1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
