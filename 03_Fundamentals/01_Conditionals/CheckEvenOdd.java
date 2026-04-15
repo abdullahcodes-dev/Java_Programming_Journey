c6c5b7c537cb21ace5df292d50b697cfc4428320
Muhammad Abdullah <mabdullah20089@gmail.com>
2026-04-15 20:55:16 +0500

    Added description of the problem (Check Even-Odd Number)

--- 03_Fundamentals/01_Conditionals/CheckEvenOdd.java
+++ 03_Fundamentals/01_Conditionals/CheckEvenOdd.java
@@ -1,6 +1,35 @@
+/*
+ * ============================================================
+ * Topic         : Conditionals
+ * Problem       : Check Even or Odd Number
+ *
+ * Description:
+ * Write a program that takes an integer as input and checks
+ * whether the number is even or odd.
+ *
+ * Logic:
+ * A number is even if it is divisible by 2, otherwise it is odd.
+ *
+ * Concepts Used:
+ * - If-else statements
+ * - Modulus operator (%)
+ * - User input (Scanner)
+ *
+ * Example:
+ * Input:
+ *  Enter a number: 4
+ *
+ * Output:
+ *  The number is EVEN
+ *
+ * Author: Muhammad Abdullah
+ * ============================================================
+ */
+
 import java.util.Scanner;
 
 public class CheckEvenOdd {
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
