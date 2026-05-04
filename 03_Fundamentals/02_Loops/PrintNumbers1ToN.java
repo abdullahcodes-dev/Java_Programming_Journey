f870c5e40f9b72b25ab41fc48a2113f6b8ee0f3f
Muhammad Abdullah <mabdullah20089@gmail.com>
2026-05-04 21:47:21 +0500

    Fundamentals: Print numbers from 1 to N, added return keyword

--- 03_Fundamentals/02_Loops/PrintNumbers1ToN.java
+++ 03_Fundamentals/02_Loops/PrintNumbers1ToN.java
@@ -1,34 +1,3 @@
-/*
- * ============================================================
- * Topic       : Loops
- * Problem     : Print Numbers from 1 to N
- *
- * Description:
- * Write a program that takes a number as input and prints
- * all numbers from 1 to that number.
- *
- * Logic:
- * - Take input n from the user
- * - If n <= 0 → Invalid input
- * - Otherwise, use a loop from 1 to n
- * - Print each number in sequence
- *
- * Concepts Used:
- * - for loop
- * - if-else condition
- * - User input (Scanner)
- *
- * Example:
- * Input:
- * Enter a number: 5
- *
- * Output:
- * 1 2 3 4 5
- *
- * Author: Muhammad Abdullah
- * ============================================================
- */
-
 import java.util.Scanner;
 
 public class PrintNumbers1ToN {
@@ -41,6 +10,7 @@ public class PrintNumbers1ToN {
 
         if (n <= 0) {
             System.out.println("Invalid input");
+            return;
         } else {
             for (int i = 1; i <= n; i++) {
                 System.out.print(i + " ");
